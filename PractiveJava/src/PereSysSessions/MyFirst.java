5ffb65e4ba72925aef141af20dadd28d0b577d39
Acer <Acer@LAPTOP-EFHQ2V7G>
2020-01-15 00:58:55 +0530

    New class in PereSysSession

--- PractiveJava/src/PereSysSessions/MyFirst.java
+++ PractiveJava/src/PereSysSessions/MyFirst.java
@@ -5,6 +5,9 @@ public class MyFirst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Thanks");
+		int a = 123;
+		a++;
+		System.out.println(a);
 	}
 
 }
